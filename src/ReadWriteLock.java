16455b3606d2e82f0b97304bca57e07c4959da25
lintao cui <lintaocui@gmail.com>
2016-01-09 18:47:17 -0800

    Add ReadWriteLock

--- src/ReadWriteLock.java
+++ src/ReadWriteLock.java
@@ -2,4 +2,37 @@
  * Created by lintaocui on 1/3/16.
  */
 public class ReadWriteLock {
+    int reader;
+    int writer;
+    int writeRequest;
+
+    public synchronized void lockRead() throws InterruptedException {
+        while(writer > 0 || writeRequest > 0)
+        {
+            wait();
+        }
+
+        reader++;
+    }
+
+    public synchronized void unlockRead() {
+        reader--;
+        notifyAll();
+    }
+
+    public synchronized void lockWrite() throws InterruptedException {
+        writeRequest++;
+
+        while(reader > 0)
+            wait();
+
+        writeRequest--;
+        writer++;
+    }
+
+
+    public synchronized void unlockWrite() {
+        writer--;
+        notifyAll();
+    }
 }
